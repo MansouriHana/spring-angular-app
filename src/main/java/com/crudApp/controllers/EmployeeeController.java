25cc577324a5996c5942e19b828f1efbb413d5a3
MansouriHana <mansouriii.hanaa@gmail.com>
2021-05-31 22:45:32 +0100

    add a new user

--- src/main/java/com/crudApp/controllers/EmployeeeController.java
+++ src/main/java/com/crudApp/controllers/EmployeeeController.java
@@ -3,8 +3,11 @@ package com.crudApp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudApp.model.Employee;
@@ -17,8 +20,15 @@ public class EmployeeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	@GetMapping("/employeeList")
+	@GetMapping("/employees")
 	public List<Employee> getEmployeeList(){
 		return employeeRepository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@Validated @RequestBody Employee employee) {
+		System.out.println("email >>>> "+employee.getEmailId());
+		//return employeeRepository.save(employee);
+		return null;
 	}
 }
