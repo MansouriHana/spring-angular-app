21546773c4a076aab6f3979d7ce83606082fb252
MansouriHana <mansouriii.hanaa@gmail.com>
2021-05-31 22:44:09 +0100

    add user part one

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../modeles/employee';
+import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  employee: Employee = new Employee();
+  submitted = false;
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  gotoList() {
+    this.router.navigate(["/employees"]);
+  }
+  save() {
+    console.log("save >>>>>>>>>>>>>>>>>>>>>>>>")
+    console.log(JSON.stringify(this.employee))
+    /*this.employeeService.createEmployee(this.employee).subscribe(data => {
+      
+      console.log(data);
+     // this.employee = new Employee();
+      //this.gotoList();
+    },
+      error => console.log("error => ", error)
+    );*/
+  }
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -7,24 +7,27 @@ import { Employee } from '../modeles/employee';
 })
 export class EmployeeService {
   employees!: Observable<Employee[]>;
-  private baseUrl="http://localhost:8080/employeeList";
+  private baseUrl = "http://localhost:8080/employees";
 
-  constructor(private httpClient: HttpClient) { 
-   /* this.employees=[
-      {emailId
-        id:1,
-        firstname:"Hana",
-        lastname:"Mansouri",
-        emailId:"Mansouriii.hanaa@gmail.com",
-        active:true
-      }
-    ]*/
+  constructor(private httpClient: HttpClient) {
+    /* this.employees=[
+       {emailId
+         id:1,
+         firstname:"Hana",
+         lastname:"Mansouri",
+         emailId:"Mansouriii.hanaa@gmail.com",
+         active:true
+       }
+     ]*/
   }
-  
-  getEmployeesList(): Observable<any>{
+
+  getEmployeesList(): Observable<any> {
     return this.httpClient.get(`${this.baseUrl}`);
-   
   }
 
+
+  createEmployee(employee: Object): Observable<Object> {
+    return this.httpClient.post(`${this.baseUrl}`, employee);
+  }
 
 }
